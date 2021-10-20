835ab03f522629c6139a6591de22d27b668e87a6
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-10-20 13:01:50 +0100

    no message

--- cloud-function/config.py
+++ cloud-function/config.py
@@ -8,12 +8,12 @@ global credentials
 def establish_gcp_credentials():
     global credentials
 
-#     if is_cloud_run:
-      credentials = GoogleCredentials.get_application_default()
-#     else:
+#     if not is_cloud_run:
 #         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials/credentials.json"
 #         credentials_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 #         credentials = service_account.Credentials.from_service_account_file(credentials_path)
+#     else:
+    credentials = GoogleCredentials.get_application_default()
 
 
 def get_env(name: str, default: str) -> str:
